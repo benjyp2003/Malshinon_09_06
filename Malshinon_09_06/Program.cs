e2c6679133a0b59fb47807fa0ee75e8fcdc74e34
benjyp2003 <benjypfeffer@gmail.com>
2025-06-09 09:23:56 +0300

    Add project files.

---
+++ Malshinon_09_06/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon_09_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
